29a3ece6bd1b72726cd685d041a5cfa78699ae3c
nmejia93 <norbeymejiacortes@gmail.com>
2025-03-14 18:34:41 -0500

    UPDATE - structure of proyect

---
+++ src/BookApi.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const BookApi = () => {
+
+    return (
+        <div>
+            <h1>BookApi voy melo</h1>
+        </div>
+    );
+}
+
+
+export default BookApi;
